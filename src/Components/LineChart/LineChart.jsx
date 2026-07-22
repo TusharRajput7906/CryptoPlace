af0b4bafb05626329bc59e3186e5f288a999ae44
TusharRajput7906 <tusharrajput2023@gmail.com>
2026-07-23 01:31:29 +0530

    Responsive and search

---
+++ src/Components/LineChart/LineChart.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Chart from 'react-google-charts'
+
+const LineChart = () => {
+
+const [data, setData] = useState([["Date","Prices"]]);
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LineChart
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,28 @@
-import React, { useContext ,useEffect,useState} from 'react'
-import './Home.css'
-import { CoinContext } from '../../Context/CoinContext'
+import React, { useContext ,useEffect,useState} from 'react';
+import './Home.css';
+import { CoinContext } from '../../Context/CoinContext';
+import { Link } from 'react-router';
 
 const Home = () => {
 
 const {allCoin,currency} = useContext(CoinContext);
 const [displayCoin, setDisplayCoin] = useState([]);
+const [input, setInput] = useState('');
+
+const inputHandler=(event)=>{
+  setInput(event.target.value);
+  if(event.target.value===''){
+    setDisplayCoin(allCoin);
+  }
+}
+const searchHandler= async (event)=>{
+  event.preventDefault();
+const coins=  await allCoin.filter((item)=>{
+   return item.name.toLowerCase().includes(input.toLowerCase());
+  })
+  setDisplayCoin(coins);
+}
+
 useEffect(()=>{
   setDisplayCoin(allCoin);
 },[allCoin]);
@@ -19,8 +36,16 @@ useEffect(()=>{
         <p>
           Welcome to the world's largest cryptocurrency marketplace. Sign up to explore more about cryptos.
         </p>
-        <form>
-          <input type='text' placeholder='Search crypto..' />
+        <form onSubmit={searchHandler}>
+
+          <input onChange={inputHandler} list='coinlist' value={input} type='text' placeholder='Search crypto..' required/>
+
+          <datalist id='coinlist'>
+            {allCoin.map((item,index)=>(
+              <option key={index} value={item.name}/>
+            ))}
+          </datalist>
+
           <button type='submit'>Search</button>
         </form>
       </div>
@@ -35,7 +60,7 @@ useEffect(()=>{
         </div>
         {
           displayCoin.slice(0,10).map((item,index)=>(
-            <div className="table-layout" key={index}>
+            <Link to={`/coin/${item.id}`} className="table-layout" key={index}>
               <p>{item.market_cap_rank}</p>
               <div>
                 <img src={item.image} alt="" />
@@ -44,7 +69,7 @@ useEffect(()=>{
               <p>{currency.symbol} {item.current_price.toLocaleString()}</p>
               <p className={item.price_change_percentage_24h>0?"green":"red"}>{Math.floor(item.price_change_percentage_24h*100)/100}</p>
               <p className='market-cap'>{currency.symbol}{item.market_cap.toLocaleString()}</p>
-            </div>
+            </Link>
           ))
         }
       </div>
